c5edb1e0d83889b1cd5b0a75c006813c33b7edb3
Behaa_Jumaa <behaa@arch.localdomain>
2020-02-18 05:36:42 +0200

    slide 27

--- project setup/back/src/index.js
+++ project setup/back/src/index.js
@@ -2,6 +2,8 @@ import app from './app'
 
 app.get( '/', (req, res) => res.send("hellllo") );
 
-app.listen( 8080, () => console.log('server listening on port 8050') )
+app.listen( 8080, () => console.log('server listening on port 8080') )
 
+
+import db from './db'
 
